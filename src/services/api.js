4e0c3a0c5e8082dd6dd3b2bcbc0d647baa1d70af
Michael Moura <mikemorcerf@gmail.com>
2020-05-03 17:37:22 -0400

    create env variables for api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://diary-app-nodejs-api.herokuapp.com/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
