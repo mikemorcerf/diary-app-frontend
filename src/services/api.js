3acbe3366d2af624b1d0710235c55ea0ce7394cf
Michael Moura <mikemorcerf@gmail.com>
2020-05-03 17:26:37 -0400

    change api baseUrl for heroku deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL: 'https://diary-app-nodejs-api.herokuapp.com/api',
 });
 
 export default api;
