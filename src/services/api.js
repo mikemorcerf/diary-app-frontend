bf1cfe4e65c6e822a7565e183cb2f64bf34aea8e
Michael Moura <mikemorcerf@gmail.com>
2020-06-11 11:26:08 -0400

    update baseurl to access backend in production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://be-the-hero-backend-js.herokuapp.com/',
+  baseURL: 'https://be-the-hero-backend-js.herokuapp.com/api',
 });
 
 export default api;
